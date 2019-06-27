e36b63d9d47337c259a0298032382e344bab2d97
DianaKun17 <dianochka.kun@mail.ru>
2019-06-27 18:08:39 +0300

    сделала номер 3

--- z1.cpp
+++ z1.cpp
@@ -1,45 +1,9 @@
-#include <string>
-#include <iostream>
-#include <cstdlib>
-using namespace std;
-int main () {
-	int a=0, 
-	    e=0, 
-		i=0, 
-		o=0, 
-		u=0, 
-		y=0;
-	string glas;
-	getline (cin,glas);
-	
-	for (int m=0;
-	m<glas.length(); m++)
-	{
-		if (glas[m]=='a') {
-			a++;
-		}
-		if (glas[m]=='e') {
-			e++;
-		}
-		if (glas[m]=='i') {
-			i++;
-		}
-		if (glas[m]=='o') {
-			o++;
-		}
-		if (glas[m]=='u') {
-			u++;
-		}
-		if (glas[m]=='y') {
-			y++;
+string s;
+	    getline(cin, s);
+      for (int i = 0; i < s.length(); i++) {
+	                   if (s[i] == 'A' || s[i] == 'E' || s[i] == 'O' || s[i] == 'I' || s[i] == 'U' || s[i] == 'Y') {
+			s[i] = tolower(s[i]);
 		}
 	}
-	cout<< "a = " << a <<endl;
-	cout<< "e = " << e <<endl;
-	cout<< "i = " << i <<endl;
-	cout<< "o = " << o <<endl;
-	cout<< "u = " << u <<endl;
-	cout<< "y = " << y <<endl;
-	
+	cout<<s<<endl;
 	return 0;
-}
